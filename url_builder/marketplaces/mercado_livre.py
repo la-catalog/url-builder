88f23f5f807e00462a933bf27e289f5f253ee353
thiagola92 <thiagola92@gmail.com>
2022-08-24 20:39:16 -0300

    Add mercado livre template

---
+++ url_builder/marketplaces/mercado_livre.py
@@ -0,0 +1,9 @@
+from url_builder.abstractions import Marketplace
+
+
+class MercadoLivre(Marketplace):
+    def build_sku_url(self, code: str) -> str:
+        return f""
+
+    def build_query_url(self, query: str) -> str:
+        return f""
